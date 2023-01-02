f3698522fa2ab01943d9e6afca9dfdeb8e6c4948
oman <omanapple42@hotmail.com>
2023-01-02 12:37:11 -0300

    init app

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Amazona
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App
